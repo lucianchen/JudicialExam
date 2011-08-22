78b63c2d4fc3ca0203edc2ae1174cd79dec96bb8
Liang Chen <lucianchen1986@126.com>
2011-08-22 22:26:18 +0800

    add new project

---
+++ src/com/lucianchen/judicialExam/JudicialExamActivity.java
@@ -0,0 +1,13 @@
+package com.lucianchen.judicialExam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JudicialExamActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
