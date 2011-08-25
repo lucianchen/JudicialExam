de13af24d805095247d9b45ccbda82b055eab346
Liang <lucianchen1986@126.com>
2011-08-25 18:29:31 +0800

    Add database file and new entity

---
+++ src/com/lucianchen/judicialExam/model/Options.java
@@ -0,0 +1,7 @@
+package com.lucianchen.judicialExam.model;
+
+import android.provider.BaseColumns;
+
+public class Options implements BaseColumns {
+	public static final String OPTION_TEXT = "optionText";
+}
--- src/com/lucianchen/judicialExam/model/Questions.java
+++ src/com/lucianchen/judicialExam/model/Questions.java
@@ -3,5 +3,7 @@ package com.lucianchen.judicialExam.model;
 import android.provider.BaseColumns;
 
 public class Questions implements BaseColumns {
+	public static final String TITLE = "title";
+	public static final String WEIGHT = "weight";
 	
 }
